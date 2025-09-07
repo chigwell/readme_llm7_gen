0f7252efdfe48bf916a37a8d4a1fe8e3416886db
Eugene Evstafev <ee345@cam.ac.uk>
2025-09-07 14:53:39 +0100

    docs: update long_description

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='readme_llm7_gen',
-    version='2025.9.71415',
+    version='2025.9.71453',
     author='Eugene Evstafev',
     author_email='hi@eugene.plus',
     description='A package for generating README.md files using LLMs',
-    long_description='',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/chigwell/readme_llm7_gen',
     packages=find_packages(),
